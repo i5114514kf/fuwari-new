abbc27d13cfe615a075fd9a8ef0c3b0cc8753e7f
i5114514kf <xiweilu15@gmail.com>
2025-10-05 16:30:53 +0800

    项目初始化

--- src/config.ts
+++ src/config.ts
@@ -8,35 +8,33 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
-	lang: "en", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
+	title: "CPer's Blog",
+	subtitle: "test",
+	lang: "zh_CN", // 语言代码，例如 'en', 'zh_CN', 'ja' 等
 	themeColor: {
-		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: false, // Hide the theme color picker for visitors
+		hue: 250, // 主题色的默认色相，范围 0 到 360。例如 red: 0, teal: 200, cyan: 250, pink: 345
+		fixed: false, // 隐藏访客的主题色选择器
 	},
 	banner: {
-		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
+		enable: true,
+		src: "assets/images/demo-banner.png", // 相对于 /src 目录。如果以 '/' 开头则相对于 /public 目录
+		position: "center", // 等同于 object-position，仅支持 'top', 'center', 'bottom'，默认为 'center'
 		credit: {
-			enable: false, // Display the credit text of the banner image
-			text: "", // Credit text to be displayed
-			url: "", // (Optional) URL link to the original artwork or artist's page
+			enable: false, // 是否显示横幅图片的版权信息
+			text: "", // 要显示的版权文本
+			url: "", // （可选）原作或作者页面的链接
 		},
 	},
 	toc: {
-		enable: true, // Display the table of contents on the right side of the post
-		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
+		enable: true, // 在文章右侧显示目录
+		depth: 2, // 目录显示的最大标题深度，范围 1 到 3
 	},
-	favicon: [
-		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
-	],
+favicon: [    // Leave this array empty to use the default favicon
+     {
+       src: 'https://img.cdn1.vip/i/68e22ab22a84e_1759652530.webp',    // Path of the favicon, relative to the /public directory
+       //theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+       sizes: '128x128',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+     }	],
 };
 
 export const navBarConfig: NavBarConfig = {
@@ -46,34 +44,39 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
-			external: true, // Show an external link icon and will open in a new tab
+			url: "https://github.com/i5114514kf", // 内部链接不应包含基础路径，系统会自动添加
+			external: true, // 显示外链图标并在新标签页打开
 		},
 	],
 };
 
 export const profileConfig: ProfileConfig = {
-	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	avatar: "assets/images/demo-avatar.png", // 相对于 /src 目录。如果以 '/' 开头则相对于 /public 目录
+	name: "CPer",
+	bio: "我的生命如同那散落之花，尽管回不去的日子屈指可数",
 	links: [
 		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-			// You will need to install the corresponding icon set if it's not already included
+			name: "Telegram",
+			icon: "fa6-brands:telegram", // 图标代码可在 https://icones.js.org/ 查询
+			// 如未包含对应图标集需手动安装
 			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
+			url: "https://t.me/lu1145",
 		},
 		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
+			name: "QQ",
+			icon: "fa6-brands:qq",
+			url: "https://qm.qq.com/cgi-bin/qm/qr?k=_ZzZ0TDMddX-aq2DD-a5yeHq36UJK2Ke",
 		},
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/i5114514kf",
 		},
+		{
+      name: 'Email',
+      icon: 'fa6-solid:envelope',
+      url: 'mailto:3550810388@qq.com',
+    },
 	],
 };
 
@@ -84,7 +87,7 @@ export const licenseConfig: LicenseConfig = {
 };
 
 export const expressiveCodeConfig: ExpressiveCodeConfig = {
-	// Note: Some styles (such as background color) are being overridden, see the astro.config.mjs file.
-	// Please select a dark theme, as this blog theme currently only supports dark background color
+	// 注意：部分样式（如背景色）已被覆盖，详见 astro.config.mjs 文件。
+	// 请务必选择深色主题，因为本博客主题目前仅支持深色背景
 	theme: "github-dark",
 };
---
+++ src/content/posts/test.md
@@ -0,0 +1,10 @@
+---
+title: test
+published: 2025-10-05
+description: ''
+image: ''
+tags: []
+category: ''
+draft: false 
+lang: ''
+---
